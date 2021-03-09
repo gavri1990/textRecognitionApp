be00a65b55875ca38e9bf5107b85c214a8513807
giorgos <giorgosgav1990@outlook.com.gr>
2021-03-09 20:54:11 +0200

    Second commit

--- app/src/main/java/com/unipi/giorgosgav/textrecognition/MainActivity.java
+++ app/src/main/java/com/unipi/giorgosgav/textrecognition/MainActivity.java
@@ -1,5 +1,6 @@
 package com.unipi.giorgosgav.textrecognition;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,12 +11,24 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     ImageButton loadPhotoButton, recognizeTextButton;
     ImageView imageView;
     ListView listView;
+    Bitmap bitmapImage;
+    List<String> textRecognitionList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
         recognizeTextButton = findViewById(R.id.recognizeTextButton);
         imageView = findViewById(R.id.imageView);
         listView = findViewById(R.id.listView);
+
+        textRecognitionList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, textRecognitionList);
+        listView.setAdapter(adapter); //"κολλάμε" τον adapter στη listview
     }
 
     @Override
@@ -44,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             Uri imageUri = data.getData();
             try
             {
-                Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
                 imageView.setImageBitmap(bitmapImage);
             }
             catch(IOException e)
@@ -63,6 +84,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void recognizeText(View view)
     {
+        InputImage image = InputImage.fromBitmap(bitmapImage, 0);
+        TextRecognizer textRecognizer = TextRecognition.getClient();
 
+        Task<Text> result =
+                textRecognizer.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text visionText) {
+                                Toast.makeText(MainActivity.this, "Text recognition completed successfully", Toast.LENGTH_LONG).show();
+                                String resultText = visionText.getText();
+                                textRecognitionList.add(resultText);
+                                adapter.notifyDataSetChanged();
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(MainActivity.this, "Text recognition failed", Toast.LENGTH_LONG).show();
+                                        e.printStackTrace();
+                                    }
+                                });
     }
 }
